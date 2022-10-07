024ad1a394bf01575e4293b36c8026f648a5ab34
Max Crone <code@maxcrone.org>
2022-10-07 09:43:34 +0200

    Finish ActionKV

--- chapter-7/actionkv/src/lib.rs
+++ chapter-7/actionkv/src/lib.rs
@@ -6,11 +6,13 @@ use std::io::{BufReader, BufWriter, SeekFrom};
 use std::path::Path;
 
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
-use crc::crc32;
+use crc::{Crc, CRC_32_ISCSI};
 use serde_derive::{Deserialize, Serialize};
 
 type ByteString = Vec<u8>;
 type ByteStr = [u8];
+
+pub const CASTAGNOLI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct KeyValuePair {
@@ -50,7 +52,7 @@ impl ActionKV {
         
         debug_assert_eq!(data.len(), data_len as usize);
 
-        let checksum = crc32::checksum_ieee(&data);
+        let checksum = CASTAGNOLI.checksum(&data);
         if checksum != saved_checksum {
             panic!(
                 "data corruption encountered ({:08x} != {:08x})",
@@ -86,5 +88,79 @@ impl ActionKV {
         }
 
         Ok(())
+    }
+
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        let position = match self.index.get(key) {
+            None => return Ok(None),
+            Some(position) => *position,
+        };
+
+        let kv = self.get_at(position)?;
+        Ok(Some(kv.value))
+    }
+
+    pub fn get_at(&mut self, position: u64) -> io::Result<KeyValuePair> {
+        let mut f = BufReader::new(&mut self.f);
+        f.seek(SeekFrom::Start(position))?;
+        let kv = ActionKV::process_record(&mut f)?;
+
+        Ok(kv)
+    }
+
+    pub fn insert(
+        &mut self,
+        key: &ByteStr,
+        value: &ByteStr,
+    ) -> io::Result<()> {
+        let position = self.insert_but_ignore_index(key, value)?;
+
+        self.index.insert(key.to_vec(), position);
+        Ok(())
+    }
+
+    pub fn insert_but_ignore_index(
+        &mut self,
+        key: &ByteStr,
+        value: &ByteStr,
+    ) -> io::Result<u64> {
+        let mut f = BufWriter::new(&mut self.f);
+        let key_len = key.len();
+        let val_len = value.len();
+        let mut tmp = ByteString::with_capacity(key_len + val_len);
+
+        for byte in key {
+            tmp.push(*byte);
+        }
+
+        for byte in value {
+            tmp.push(*byte);
+        }
+
+        let checksum = CASTAGNOLI.checksum(&tmp);
+
+        let next_byte = SeekFrom::End(0);
+        let current_position = f.seek(SeekFrom::Current(0))?;
+        f.seek(next_byte)?;
+        f.write_u32::<LittleEndian>(checksum)?;
+        f.write_u32::<LittleEndian>(key_len as u32)?;
+        f.write_u32::<LittleEndian>(val_len as u32)?;
+        f.write_all(&tmp)?;
+
+        Ok(current_position)
+    }
+
+    #[inline]
+    pub fn update(
+        &mut self,
+        key: &ByteStr,
+        value: &ByteStr
+    ) -> io::Result<()> {
+        self.insert(key, value)
+    }
+
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
     }
 }

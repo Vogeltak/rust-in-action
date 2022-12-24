ce60a350883fc9d9ed2ec31cb89ba27823e4f15d
Max Crone <code@maxcrone.org>
2022-12-24 22:27:38 +0100

    Finish chapter 11

--- chapter-11/fledgeos/src/main.rs
+++ chapter-11/fledgeos/src/main.rs
@@ -1,26 +1,105 @@
 #![no_std]
 #![no_main]
 #![feature(core_intrinsics)]
+#![feature(lang_items)]
 
-use core::intrinsics;
+use core::fmt;
 use core::panic::PanicInfo;
+use core::fmt::Write;
 use x86_64::instructions::hlt;
+
+#[allow(unused)]
+#[derive(Clone, Copy)]
+#[repr(u8)]
+enum Color {
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    Gray = 0x7,
+    DarkGray = 0x8,
+    BrightBlue = 0x9,
+    BrightGreen = 0xA,
+    BrightCyan = 0xB,
+    BrightRed = 0xC,
+    BrightMagenta = 0xD,
+    Yellow = 0xE,
+    White = 0xF,
+}
+
+struct Cursor {
+    position: isize,
+    foreground: Color,
+    background: Color,
+}
+
+impl Cursor {
+    fn color(&self) -> u8 {
+        let fg = self.foreground as u8;
+        let bg = (self.background as u8) << 4;
+        fg | bg
+    }
+
+    fn print(&mut self, text: &[u8]) {
+        let color = self.color();
+
+        let framebuffer = 0xb800 as *mut u8;
+
+        for &character in text {
+            unsafe {
+                framebuffer.offset(self.position).write_volatile(character);
+                framebuffer.offset(self.position + 1).write_volatile(color);
+            }
+            self.position += 2;
+        }
+    }
+}
+
+impl fmt::Write for Cursor {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.print(s.as_bytes());
+        Ok(())
+    }
+}
 
 #[panic_handler]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
-    intrinsics::abort();
+pub fn panic(info: &PanicInfo) -> ! {
+    let mut cursor = Cursor {
+        position: 0,
+        foreground: Color::White,
+        background: Color::Red,
+    };
+
+    // Clear the screen
+    for _ in 0..(80*25) {
+        cursor.print(b" ");
+    }
+
+    cursor.position = 0;
+    write!(cursor, "{}", info).unwrap();
+
+    loop {}
 }
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() { }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let framebuffer = 0xb8000 as *mut u8;
+    let text = b"Rust in Action";
 
-    unsafe {
-        framebuffer
-            .offset(1)
-            .write_volatile(0x30);
-    }
+    let mut cursor = Cursor {
+        position: 0,
+        foreground: Color::White,
+        background: Color::Blue,
+    };
+
+    cursor.print(text);
 
     loop {
         hlt();
